7badad2e0c74feec158caa3b7d8494112590edd2
florian-miotto <fmiotto@gmail.com>
2023-04-12 11:57:27 +0200

    ajout facebook bouton

--- testconn/lib/ConnexionPage.dart
+++ testconn/lib/ConnexionPage.dart
@@ -150,16 +150,19 @@ class _LoginPageState extends State<LoginPage> {
                   const SizedBox(height: 10),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(
-                        FontAwesomeIcons.google,
-                        color: Color.fromARGB(255, 34, 87, 104),
-                      ),
+                    children:  [
+                      GoogleBtn1(onPressed:(){}),
+                      // Icon(
+                      //   FontAwesomeIcons.google,
+                      //   color: Color.fromARGB(255, 34, 87, 104),
+                      // ),
+                      FacebookBtn1(onPressed:(){}),
                       SizedBox(width: 10),
-                      Icon(
-                        FontAwesomeIcons.facebook,
-                        color: Color.fromARGB(255, 34, 87, 104),
-                      ),
+                      
+                      // Icon(
+                      //   FontAwesomeIcons.facebook,
+                      //   color: Color.fromARGB(255, 34, 87, 104),
+                      // ),
                     ],
                   ),
                   //text "Se connecter" + bouton de connexion (rond avec fleche vers la droite)
@@ -279,5 +282,82 @@ Row(
         _isLoading = false;
       });
     }
+  }
+}
+class FacebookBtn1 extends StatelessWidget {
+final Function() onPressed;
+  const FacebookBtn1({
+required this.onPressed,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 54,
+        margin: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+        ),
+        child: TextButton(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)))
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/crypto%2Ffacebook%20(2).png?alt=media&token=9c275bf0-2bf7-498a-9405-9ae99df8d8f2",
+                width: 20,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Text("Facebook",
+                  style: TextStyle(color: Colors.black, fontSize: 16)),
+            ],
+          ),
+          onPressed: onPressed,
+        ));
+  }
+}
+
+class GoogleBtn1 extends StatelessWidget {
+final Function() onPressed;
+  const GoogleBtn1({
+required this.onPressed,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 54,
+        margin: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+        ),
+        child: TextButton(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)))
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/crypto%2Fsearch%20(2).png?alt=media&token=24a918f7-3564-4290-b7e4-08ff54b3c94c",
+                width: 20,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Text("Google",
+                  style: TextStyle(color: Colors.black, fontSize: 16)),
+            ],
+          ),
+          onPressed: onPressed,
+        ));
   }
 }
