cb6d7c98e693f1cd07b07f7c473b8264c820358b
florian-miotto <fmiotto@gmail.com>
2023-04-12 12:00:18 +0200

    ajout bouton twitter

--- testconn/lib/ConnexionPage.dart
+++ testconn/lib/ConnexionPage.dart
@@ -157,6 +157,7 @@ class _LoginPageState extends State<LoginPage> {
                       //   color: Color.fromARGB(255, 34, 87, 104),
                       // ),
                       FacebookBtn1(onPressed:(){}),
+                      TwitterBtn1(onPressed: () {  },),
                       SizedBox(width: 10),
                       
                       // Icon(
@@ -354,6 +355,44 @@ required this.onPressed,
                 width: 10,
               ),
               const Text("Google",
+                  style: TextStyle(color: Colors.black, fontSize: 16)),
+            ],
+          ),
+          onPressed: onPressed,
+        ));
+  }
+}
+class TwitterBtn1 extends StatelessWidget {
+  final Function() onPressed;
+  const TwitterBtn1({
+    required this.onPressed,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 54,
+        margin: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+        ),
+        child: TextButton(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)))
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/socials%2Ftwitter.png?alt=media&token=abd55874-2c77-4322-88e0-aa7598ee2a10",
+                width: 20,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Text("Twitter",
                   style: TextStyle(color: Colors.black, fontSize: 16)),
             ],
           ),
